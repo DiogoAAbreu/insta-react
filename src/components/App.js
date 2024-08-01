e25708b4323857ddc0b10486dec3531612dde144
Diogo Alberto <diogoalberto.dev@gmail.com>
2024-08-01 06:45:32 -0300

    side bar concluida, removido bug onde a setinha dos storys aparecia no layout mobile

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar";
 import Corpo from "./Corpo";
+import FundoMobile from "./FundoMobile";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Corpo />
+      <FundoMobile />
     </div>
   )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,15 @@ export default function Sidebar() {
     return (
         <div className="sidebar">
             <Usuario />
+            <Sugestoes />
+            <div class="links">
+                Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
+                Hashtags • Idioma
+            </div>
+
+            <div class="copyright">
+                © 2021 INSTAGRAM DO FACEBOOK
+            </div>
         </div>
     )
 }
@@ -18,6 +27,57 @@ function Usuario() {
                     <ion-icon name="pencil"></ion-icon>
                 </span>
             </div>
+        </div>
+    )
+}
+
+function Sugestoes() {
+    return (
+        <div className="sugestoes">
+            <div class="titulo">
+                Sugestões para você
+                <div>Ver tudo</div>
+            </div>
+
+            {sugestoes.map(sugestao => <Sugestao imgSugestao={sugestao.imgSugestao} sugestao={sugestao.sugestao} />)}
+        </div>
+    )
+}
+
+const sugestoes =
+    [
+        {
+            imgSugestao: "assets/img/bad.vibes.memes.svg",
+            sugestao: "bad.vibes.memes"
+        },
+        {
+            imgSugestao: "assets/img/chibirdart.svg",
+            sugestao: "chibirdart"
+        },
+        {
+            imgSugestao: "assets/img/razoesparaacreditar.svg",
+            sugestao: "razoesparaacreditar"
+        }, {
+            imgSugestao: "assets/img/adorable_animals.svg",
+            sugestao: "adorable_animals"
+        }, {
+            imgSugestao: "assets/img/smallcutecats.svg",
+            sugestao: "smallcutecats"
+        }
+    ]
+
+function Sugestao(props) {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.imgSugestao} />
+                <div class="texto">
+                    <div class="nome">{props.sugestao}</div>
+                    <div class="razao">Segue você</div>
+                </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
         </div>
     )
 }
